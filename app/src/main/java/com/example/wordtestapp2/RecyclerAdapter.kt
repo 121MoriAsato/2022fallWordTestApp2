8b3926caa3f9729c3c3a477b99c7d18c9a14a44d
121MoriAsato <93442992+121MoriAsato@users.noreply.github.com>
2022-12-18 20:00:24 +0900

    特別開校日

--- app/src/main/java/com/example/wordtestapp2/RecyclerAdapter.kt
+++ app/src/main/java/com/example/wordtestapp2/RecyclerAdapter.kt
@@ -1,34 +1,73 @@
 package com.example.wordtestapp2
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import io.realm.kotlin.Realm
+import io.realm.kotlin.RealmConfiguration
+import io.realm.kotlin.ext.query
 
-interface SaveAsFavorite{
-    fun buttonTapped()
-}
+class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolderItem>(){
 
-class RecyclerAdapter:RecyclerView.Adapter<ViewHolderItem>() {
+    inner class ViewHolderItem(itemView: View, recyclerView: RecyclerAdapter) :
+            RecyclerView.ViewHolder(itemView){
+                private val config = RealmConfiguration.Builder(schema = setOf(Remember::class)).build()
+                private val realm:Realm = Realm.open(config)
+                val correctJapanese: TextView = itemView.findViewById(R.id.correct_japanese)
+                val correctEnglish: TextView = itemView.findViewById(R.id.correct_english)
 
+                init {
+                    //一行分のアイテムを選択したとき
+                    itemView.setOnClickListener {
+                        val position:Int = adapterPosition
+                        val listPosition = wordList[position]
 
-//    val imageList = listOf(
-//        R.drawable.cake_image,R.drawable.cat_image,
-//    )
+                        fun delete(){
+                            //     ↓これはrealm.writeBlocking{}の｛｝内で「自分でまとめたdata class（今回はRemember class）を扱って何かします.」と伝えるためにある。
+                            realm.writeBlocking {
+                                val deleteList = this.query<Remember>().find().first()
+                                delete(deleteList)
+                            }
+                        }
+                    }
+                }
+            }
+//            　　　　　　　　　　　　　　　↓これ（型を定義）で保存してる場所とつながった
+    private val wordList:MutableList<Remember> = mutableListOf()
 
+    //一行分のリストを用意
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderItem {
-
         val itemXml = LayoutInflater.from(parent.context)
             .inflate(R.layout.word_list_layout,parent,false)
-        return ViewHolderItem(itemXml)
+        return ViewHolderItem(itemXml,this)
     }
 
+    //リストのposition番目を取得
     override fun onBindViewHolder(holder: ViewHolderItem, position: Int) {
-        holder.groupNameViewHolder.text = nameList[position]
-        holder.allOfCorrectwordViewHolder.text = allOfCorrectList[position]
-        holder.favoriteButtonHolder.setBackgroundResource(R.drawable.after_change_button)
+
+        //override fun onBindViewHolderは存在するだけで中身は書かなくてもいい時はある。
+        //今回は一覧画面で「選択して消す」でどこ（position）を押したかの情報が必要だから記述する。
+
+        // Rememberのどこと対応するかを「.○○」で明示した
+        val japaneseItem = wordList[position].japanese
+        //holder: ViewHolderItemより、holderはViewHolderItemが型である。そのためViewHolderItemの中にあるcorrect○○を引っ張てこれる。
+        //また、ViewHolderItem.correct○○にできない理由は、あくまでViewHolderItemはStringやIntと同じの種類の”型”に過ぎないから。
+        holder.correctJapanese.text = japaneseItem
+
+        val englishItem = wordList[position].english
+        holder.correctEnglish.text = englishItem
+    }
+    //リストのサイズ
+    override fun getItemCount(): Int {
+        return wordList.size
     }
 
-    override fun getItemCount(): Int {
-        return nameList.size
+    //　ここまでの3個のひな形で書いたことをwordListというmutableListOf()に渡す関数
+    //　　　　　　　　　↓「addListはRememberという型のListの型だよ」
+    fun submitList(addList:List<Remember>){
+        wordList.addAll(addList)
+        notifyDataSetChanged()
     }
 }
--- app/src/main/java/com/example/wordtestapp2/WrodAddActivity.kt
+++ app/src/main/java/com/example/wordtestapp2/WrodAddActivity.kt
@@ -1,5 +1,6 @@
 package com.example.wordtestapp2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.system.Os.read
@@ -24,11 +25,23 @@ class WrodAddActivity : AppCompatActivity() {
         .build()
     val realm: Realm = Realm.open(config)
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-binding = ActivityWrodAddBinding.inflate(layoutInflater).apply { setContentView(this.root) }
-        val spinner: Spinner = findViewById(R.id.groupSpinner)
+        binding = ActivityWrodAddBinding.inflate(layoutInflater).apply { setContentView(this.root) }
+
+        binding.quizButton.setOnClickListener {
+            val quizIntent =Intent(this,MainActivity::class.java)
+            startActivity(quizIntent)
+        }
+
+        binding.returnListButton.setOnClickListener {
+            val listIntent =Intent(this,WordListActvity::class.java)
+            startActivity(listIntent)
+        }
+
+
+
+
 
         ArrayAdapter.createFromResource(
             this,
@@ -39,51 +52,60 @@ binding = ActivityWrodAddBinding.inflate(layoutInflater).apply { setContentView(
             spinner.adapter = adapter
         }
 
+
+
+       // var groupAlf: String = "None"
+
+
+
+//        // リスナーを登録
+//        binding.groupSpinner.setOnItemSelectedListener(object : AdapterView.OnItemSelectedListener {
+//            //　アイテムが選択された時
+//            override fun onItemSelected(
+//                parent: AdapterView<*>?,
+//                view: View?,
+//                position: Int,
+//                id: Long
+//            ) {
+//                val spinner = parent
+//                groupAlf = spinner?.selectedItem.toString()
+//            }
+//
+//            //　アイテムが選択されなかった
+//            override fun onNothingSelected(parent: AdapterView<*>?) {
+//                //
+//            }
+//        })
+
+        binding.saveButton.setOnClickListener {
+            val etJapanese: String = binding.japaneseEditText.text.toString()
+            val etEnglish: String = binding.englishEditText.text.toString()
+           // val example: String = binding.exampleEditText.text.toString()
+
+            //save(japanese, english, example, groupAlf)
+            save(etJapanese, etEnglish)
+        }
+    }
+
+    private fun save(wdJapanese: String, wdEnglish: String) {
+
         //　変数:○○　は変数の中に○○しか入れられない
         //rememberに入力した単語のデータが入っている
         val remember: Remember? = readAll()
-        Log.d("aaaa",remember?.japanese.toString())
 
-        var groupAlf: String = "None"
-
-
-
-        // リスナーを登録
-        binding.groupSpinner.setOnItemSelectedListener(object : AdapterView.OnItemSelectedListener {
-            //　アイテムが選択された時
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                val spinner = parent
-                groupAlf = spinner?.selectedItem.toString()
-            }
-
-            //　アイテムが選択されなかった
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                //
-            }
-        })
-
-        binding.saveButton.setOnClickListener {
-            val japanese: String = binding.japaneseEditText.text.toString()
-            val english: String = binding.englishEditText.text.toString()
-            val example: String = binding.exampleEditText.text.toString()
-
-
-            save(japanese, english, example, groupAlf)
-        }
-
-
+        //ありこさんと書き方が違う
+        remember.japanese = wdJapanese
+        remember.english = wdEnglish
     }
 
     private fun readAll(): Remember? {
+        //: RealmResults<Remember>は「今からRememberの中から検索しますよ～」と伝えている。
+        // 　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　↓queryはRememberの中の何かを（）内に書いて持ってくる
         val rememberResult: RealmResults<Remember> = realm.query<Remember>().find()
         return Remember()
     }
 
+    //頭文字が大文字なのを治す
     private fun save(Sjapanese: String, Senglish: String, Sexample: String, groupAlf: String) {
 
         //update
@@ -103,7 +125,6 @@ binding = ActivityWrodAddBinding.inflate(layoutInflater).apply { setContentView(
                 group = groupAlf
             })
         }
-
 
 
 }
